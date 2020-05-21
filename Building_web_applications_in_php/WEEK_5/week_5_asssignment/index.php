da58976dafe3e6bf70669190ecba4eb564c00c5b
anish <badeanish6045@gmail.com>
2020-05-21 12:33:44 +0545

    5/21

---
+++ Building_web_applications_in_php/WEEK_5/week_5_asssignment/index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>Anish Bade</title>
+</head>
+<body>
+<h1>Anish Bade</h1>
+
+<?php
+print hash('sha256', 'Anish Bade');
+?>
+<pre>
+    
+        ***
+       ** **
+      **   **
+     *********
+    **       **
+   **         ** 
+</body>
+</html>
--- first.php
+++ first.php
@@ -1,5 +1,6 @@
 <h1>html tag</h1>
 <?php
-echo"this is php text"
+echo"this is php text\n";
+print hash('md5', 'AZ9d');
 ?>
 <p>this is html paragraph</p>
